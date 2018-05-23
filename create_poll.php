43fecf7d574f3b8e4e8551bb961bc97a3080b131
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-05-23 17:17:28 +0200

    aaa

--- create_poll.php
+++ create_poll.php
@@ -5,31 +5,30 @@ $id_poll=rand(10000000, 99999999);
 $sql = 'SELECT * FROM polls WHERE id='.$id_poll;
 $result = $bdd->query($sql);
 
-while ($result->num_rows > 0) {
+while ($result->rowCount() > 0) {
 	$id_poll=rand(10000000, 99999999);
 	$sql = 'SELECT * FROM polls WHERE id='.$id_poll;
 	$result = $bdd->query($sql);
 }
 
 $creator_Id = NULL;
-while($row = $result->fetch_assoc()) {
+while($row = $result->fetchColumn()) {
       $creator_Id=$row['id'];
 }
 
 $sql = 'SELECT id FROM user WHERE username='.$_POST['pseudo'];
 $result = $bdd->query($sql);
 
-$sql = 'INSERT INTO polls(id, creatorId, question) VALUES ('.$id_poll', '.$creator_Id', '.$_POST['question']);
+$sql = 'INSERT INTO polls(id, creatorId, question) VALUES ('.$id_poll.', '.$creator_Id.', '.$_POST['question'].')';
 $bdd->exec($sql);
 
-$sql = 'INSERT INTO answers(pollId, answer) VALUES ('.$creator_Id', '.$_POST['choice1']);
+$sql = 'INSERT INTO answers(pollId, answer) VALUES ('.$creator_Id.', '.$_POST['choice1'].')';
 $bdd->exec($sql);
 
-$sql = 'INSERT INTO answers(pollId, answer) VALUES ('.$creator_Id', '.$_POST['choice2']);
+$sql = 'INSERT INTO answers(pollId, answer) VALUES ('.$creator_Id.', '.$_POST['choice2'].')';
 $bdd->exec($sql);
 
-$sql = 'INSERT INTO answers(pollId, answer) VALUES ('.$creator_Id', '.$_POST['choice3']);
+$sql = 'INSERT INTO answers(pollId, answer) VALUES ('.$creator_Id.', '.$_POST['choice3'].')';
 $bdd->exec($sql);
 
-$bdd->close();
 ?>
