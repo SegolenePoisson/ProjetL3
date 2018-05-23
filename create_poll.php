f0c9199ec4697c5e5d2c1a3fac4f02bf1179e70e
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-05-23 16:32:50 +0200

    v2

--- create_poll.php
+++ create_poll.php
@@ -5,13 +5,18 @@ id_poll=rand(10000000, 99999999);
 $sql = "SELECT * FROM machin WHERE id=".$id_poll;
 $result = $bdd->query($sql);
 
+creatorId = NULL;
+while($row = $result->fetch_assoc()) {
+      creatorId=$row["id"];
+}
+
 while ($result->num_rows > 0) {
 	id_poll=rand(10000000, 99999999);
 	$sql = "SELECT * FROM machin WHERE id=".$id_poll;
 	$result = $bdd->query($sql);
 }
 
-$sql = "SELECT id FROM user WHERE username= $_POST["pseudo"]";
+$sql = "SELECT id FROM user WHERE username=".$_POST["pseudo"];
 $result = $bdd->query($sql);
 
 $sql = INSERT INTO polls(id_poll, creatorId, question) VALUES ([value-1],[value-2],[value-3]);
