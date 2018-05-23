97ea1d5c267a290cfaa98b550460dafbeecddfb1
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-05-23 16:51:26 +0200

    v3

--- create_poll.php
+++ create_poll.php
@@ -2,24 +2,34 @@
 $bdd = new PDO('mysql:host=localhost;dbname=poll;charset=utf8', 'root', '');
 
 id_poll=rand(10000000, 99999999);
-$sql = "SELECT * FROM machin WHERE id=".$id_poll;
+$sql = "SELECT * FROM polls WHERE id=".$id_poll;
 $result = $bdd->query($sql);
-
-creatorId = NULL;
-while($row = $result->fetch_assoc()) {
-      creatorId=$row["id"];
-}
 
 while ($result->num_rows > 0) {
 	id_poll=rand(10000000, 99999999);
-	$sql = "SELECT * FROM machin WHERE id=".$id_poll;
+	$sql = "SELECT * FROM polls WHERE id=".$id_poll;
 	$result = $bdd->query($sql);
+}
+
+creator_Id = NULL;
+while($row = $result->fetch_assoc()) {
+      creator_Id=$row["id"];
 }
 
 $sql = "SELECT id FROM user WHERE username=".$_POST["pseudo"];
 $result = $bdd->query($sql);
 
-$sql = INSERT INTO polls(id_poll, creatorId, question) VALUES ([value-1],[value-2],[value-3]);
+question_poll=$_POST["question"];
+$sql = INSERT INTO polls(id, creatorId, question) VALUES (id_poll, creator_Id, question_poll);
+
+answer1_poll=$_POST["choice1"];
+$sql = INSERT INTO answers(pollId, answer) VALUES (creator_Id, answer1_poll);
+
+answer2_poll=$_POST["choice2"];
+$sql = INSERT INTO answers(pollId, answer) VALUES (creator_Id, answer2_poll);
+
+answer3_poll=$_POST["choice3"];
+$sql = INSERT INTO answers(pollId, answer) VALUES (creator_Id, answer3_poll);
 
  $bdd->exec($sql);
  $bdd->close();
