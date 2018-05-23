91afc3fada700616a2e2755d802d5519a94558bb
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-05-23 16:26:32 +0200

    test

---
+++ create_poll.php
@@ -0,0 +1,21 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=poll;charset=utf8', 'root', '');
+
+id_poll=rand(10000000, 99999999);
+$sql = "SELECT * FROM machin WHERE id=".$id_poll;
+$result = $bdd->query($sql);
+
+while ($result->num_rows > 0) {
+	id_poll=rand(10000000, 99999999);
+	$sql = "SELECT * FROM machin WHERE id=".$id_poll;
+	$result = $bdd->query($sql);
+}
+
+$sql = "SELECT id FROM user WHERE username= $_POST["pseudo"]";
+$result = $bdd->query($sql);
+
+$sql = INSERT INTO polls(id_poll, creatorId, question) VALUES ([value-1],[value-2],[value-3]);
+
+ $bdd->exec($sql);
+ $bdd->close();
+?>
