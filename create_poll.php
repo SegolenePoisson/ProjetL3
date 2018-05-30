a2fb4c8b8e6d156872403294f1f4d7fb13e6625d
Myde <Mydexios.ms@gmail.com>
2018-05-30 14:53:08 +0200

    a

--- create_poll.php
+++ create_poll.php
@@ -3,25 +3,28 @@ session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=poll;charset=utf8', 'root', '');
 
 $id_poll=rand(10000000, 99999999);
+echo $id_poll;
 $sql = 'SELECT * FROM polls WHERE id=?';
 $result = $bdd->prepare($sql);
 $result->execute([$id_poll]);
 
 while ($result->rowCount() > 0) {
   $id_poll=rand(10000000, 99999999);
+  echo $id_poll;
   $sql = 'SELECT * FROM polls WHERE id=?';
   $result = $bdd->prepare($sql);
   $result->execute([$id_poll]);
 }
 
 $creator_Id = NULL;
-while($row = $result->fetchColumn()) {
-  $creator_Id=$row['id'];
-}
+
 
 $sql = 'SELECT id FROM user WHERE username=?';
 $result = $bdd->prepare($sql);
 $result->execute([$_SESSION['pseudo']]);
+while($row = $result->fetchColumn()) {
+  $creator_Id=$row['id'];
+}
 
 $sql = 'INSERT INTO polls(id, creatorId, question) VALUES (?, ?, ?)';
 $result = $bdd->prepare($sql);
