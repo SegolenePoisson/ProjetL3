ab13b2c6a2ef4360ef7dbb42e53a0520eb80c2c0
OlivoRomain <39304325+OlivoRomain@users.noreply.github.com>
2018-05-30 14:25:05 +0200

    modif nabar Strawpoll

--- navbar.php
+++ navbar.php
@@ -23,7 +23,7 @@
     <div class="container-fluid">
       <ul class="nav navbar-nav">
         <li <?php if ($_SESSION["current_page"] == "home"){ echo 'class="active"';}?> ><a href="index.php">Home</a></li>
-        <li <?php if ($_SESSION["current_page"] == "poll"){ echo 'class="active"';}?> ><a href="poll.php">StrawPoll</a></li>
+        <li <?php if ($_SESSION["current_page"] == "poll"){ echo 'class="active"';}?> ><a href="new_poll.php">StrawPoll</a></li>
         <li <?php if ($_SESSION["current_page"] == "about"){ echo 'class="active"';}?> ><a href="about.php">About Us</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
