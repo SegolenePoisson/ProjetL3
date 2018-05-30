49f054bd5a5d8052639b0004c70fc850fa0fad8e
valentinpetit <master_night_mare@hotmail.fr>
2018-05-30 14:08:33 +0200

    test ?

--- POLL.php
+++ POLL.php
@@ -1,104 +1,60 @@
 <?php
 session_start();
-$_SESSION["current_page"] = "poll";
-if(isset($_SESSION["pseudo"])){
-  ?>
-  <!DOCTYPE html>
-  <html lang="en">
-
-  <head>
-    <meta charset="utf-8">
-    <!-- For proper scaling on mobile -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- JQuery form google -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- BOOTSTRAP -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <!-- Classe css -->
-    <link rel="stylesheet" href="class1.css" />
-    <style>
-      label{
-        color: white;
-      }
-      form{
-        width: 50%; /* On a indiqué une largeur (obligatoire) */
-        margin: auto;
-        padding-top: 50px;
-        text-align: center;
-        line-height: 1.5;
-      }
-      p{
-        font-size: 12px;
-      }
-      ;
-    </style>
-    <title>WOUI</title>
-  </head>
-    
-    
-    <body>
-      <?php 
-      include 'navbar.php';
-      ?>
-      <h1>New poll</h1>
-      <form method="post" action="create_poll.php">
-   
-        <label for="question">Question* : </label>
-        <input type="text" name="question" id="question" size="35" placeholder="Ex : What's you favorite color ?" required /><br/>
-
-        <label for="choice1">Answer 1* : </label>
-        <input type="text" name="choice1" id="choice1" size="35" placeholder="Ex : Blue." required /><br/>
-
-        <label for="choice2">Answer 2* : </label>
-        <input type="text" name="choice2" id="choice2" size="35" placeholder="Ex : Red." required /><br/>
-
-        <label for="choice3">Answer 3 : </label>
-        <input type="text" name="choice3" id="choice3" size="35" placeholder="Ex : Green."/><br/>
-
-        <input type="submit" value="Create poll" />
-
-      </form>
-      <p>* this field is required.<br/></p>
-    </body>
-  </html>
-  <?php
-  }
-else{
-  ?>
-  <!DOCTYPE html>
-  <html lang="en">
-
-  <head>
-    <meta charset="utf-8">
-    <!-- For proper scaling on mobile -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- JQuery form google -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- BOOTSTRAP -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <!-- Classe css -->
-    <link rel="stylesheet" href="class1.css" />
-    <title>WOUI</title>
-  </head>  
-    <body>
-      <?php 
-      include 'navbar.php';
-      ?>
-      <h1>New poll</h1>
-      
-      <p>To create a new poll, please <a href="logIn.php">log in</a>. If you don't have an account, you can <a href="SignUp.php">create one</a> !<br/></p>
-    </body>
-  </html>
-  <?php
-}
+$_SESSION["current_page"] = "profile";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <!-- For proper scaling on mobile -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- JQuery form google -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- BOOTSTRAP -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- Classe css -->
+	<link rel="stylesheet" href="class1.css" />
+    
+	<title>WOUI</title>
+</head>
+<body>
+  
+  <?php
+  include 'navbar.php';
+  ?>
+
+<div class="container">
+  <div class="row justify-content-md-center">
+ 
+       <?php
+       if(isset($_GET['id'])) {
+       $bdd = new PDO('mysql:host=localhost;dbname=poll;charset=utf8', 'root', '');
+       echo '<form action="/action_page.php">';
+       $reponse = $bdd->query('SELECT * FROM polls WHERE polls.id =' . $_GET['id']);
+       $donnees = $reponse->fetch();
+       echo 'Question :'. $donnees['question'] .'<br>';
+
+          
+          
+            $reponse = $bdd->query('SELECT * FROM answers WHERE answers.pollId =' . $_GET['id']);
+            while ($donnees = $reponse->fetch()) {
+
+              echo '<input type="checkbox">'. $donnees['answer'] .'<br>';
+            } 
+          }
+        ?>
+        <input type="submit" value="Submit">
+      </form>
+
+      
+    </div>
+  </div>
+
+</body>
+</html>
