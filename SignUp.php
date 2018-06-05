17d253a13539e9cc163265f2515c55aeb975b440
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-06-05 12:02:35 +0200

    js in another file
    plus propre et plus "rapide"

--- SignUp.php
+++ SignUp.php
@@ -96,52 +96,7 @@ session_start();
     </div>
   </div>
   
-  <script>
-
-    function checkEmail() {
-      var email = document.getElementById("email"),
-          regexEmail = /.+@.+\..+/;
-
-      if(email.value){
-        document.getElementById("checkemail").textContent = (regexEmail.test(email.value)) ? "" : "Invalid email address";
-        document.getElementById("submit").textContent = "";
-      }
-    }
-
-    function comparePasswords() {
-      var password = document.getElementById("password");
-      var valuePassword = password.value, 
-          valueConfirm = document.getElementById("confirm").value;
-
-      if (valuePassword && valueConfirm) { // Si les deux champs contiennent quelque chose
-          document.getElementById("confirmpassword").textContent = (valuePassword === valueConfirm) ? "" : "Not the same passwords";
-          document.getElementById("submit").textContent = "";
-      } 
-    }
-        
-    var password = document.getElementById("password"),
-        confirm = document.getElementById("confirm"),
-        email = document.getElementById("email");
-      
-    if(email){
-      email.addEventListener("blur", checkEmail, false);
-    }
-
-    if (password && confirm) {
-      password.addEventListener("keyup", comparePasswords, false);
-      confirm.addEventListener("keyup", comparePasswords, false);  
-      password.addEventListener("blur", comparePasswords, false);  
-      confirm.addEventListener("blur", comparePasswords, false);  
-    }
-
-    document.querySelector("form").addEventListener("submit", function (e) {
-      if(document.getElementById("confirmpassword").textContent !== document.getElementById("checkemail").textContent){
-        e.preventDefault(); // pour empecher l'envoi si erreur dans le formulaire
-        document.getElementById("submit").textContent = "Invalid email or different passwords, please check your informations.";
-      }
-    });
-
-  </script>
+  <script src="register.js"></script>
   <p>Already an account ? <a href="logIn.php">Log in !</a></p>
 </body>
 </html>
