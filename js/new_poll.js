545de7db4f740f2e61be946a77e78b6a841a8f38
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-06-18 14:04:26 +0200

    js for option choice

--- js/new_poll.js
+++ js/new_poll.js
@@ -3,44 +3,48 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Datepicker.init(elems, options);
   }); //initialization for datepicker
 
-function setOpt(optId){
-	var opt;
-	switch (optId) {
-		case "rep":
-			opt = answers;
-		break;
-		case "texte":
-			opt = textarea;
-		break;
-		case "date":
-			opt = calendar;
-		break;
-	}
-	document.querySelectorAll("form").replaceChild(opt, document.querySelectorAll("form div"));
-}
+
 
 var textarea = document.getElementById("texte").parentNode.removeChild(document.getElementById("texte")),
 	calendar = document.getElementById("date").parentNode.removeChild(document.getElementById("date"));
 
 var answers = document.getElementById("rep").cloneNode(true);
 
+
+/*
+function setOpt(optId){
+	switch (optId) {
+		case "rep":
+			document.querySelector("form").replaceChild(answers, document.querySelector("form div"));
+		break;
+		case "texte":
+			document.querySelector("form").replaceChild(textarea, document.querySelector("form div"));
+		break;
+		case "date":
+			document.querySelector("form").replaceChild(calendar, document.querySelector("form div"));
+		break;
+}
+
+//setOpt("rep");
+*/
+
 var optrep = document.getElementById("opt_rep"),
 	opttexte = document.getElementById("opt_texte"),
 	optdate = document.getElementById("opt_date");
 
-var opt = document.querySelectorAll('input[type=radio]');
+
 optrep.addEventListener("click", function() {
 	if(optrep.checked){
-		setOpt("rep");
+		document.querySelector("form").replaceChild(answers, document.querySelector("form div"));
 	}
 });
 opttexte.addEventListener("click", function() {
 	if(opttexte.checked){
-		setOpt("texte");
+		document.querySelector("form").replaceChild(textarea, document.querySelector("form div"));
 	}
 });
 optdate.addEventListener("click", function() {
 	if(optdate.checked){
-		setOpt("date");
+		document.querySelector("form").replaceChild(calendar, document.querySelector("form div"));
 	}
 });
