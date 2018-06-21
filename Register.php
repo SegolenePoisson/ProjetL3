c1f864104f57a50166a9c26af6b395b4da825197
OlivoRomain <thegrec22@gmail.com>
2018-06-21 19:49:48 +0200

    ajout captcha + commentaire
    commentaire + francais

--- Register.php
+++ Register.php
@@ -11,22 +11,32 @@ echo "<body>";
 include 'navbar.php';
 include 'db_connect.php';
 
-if(isset($_POST["name"], $_POST["email"], $_POST["username"])){
-  $email = $_POST["email"];
-  $username = $_POST["username"];
-  $name = $_POST["name"];
 
+/* Vérification de la validité du captcha */
+if ($_POST["captcha"] != $_SESSION['captcha']){
+		echo "Mauvais captcha";
+		header("refresh:0;url=signup.php");
+}
+else{
+	/* Verifie si les champs du formulaire d'inscription ont été remplis */
+	if(isset($_POST["name"], $_POST["email"], $_POST["username"], $_POST["test"])){
 
-  include 'encryption.php';
+		$email = $_POST["email"];
+		$username = $_POST["username"];
+		$name = $_POST["name"];
+	  
+		include 'encryption.php';
 
-  $password = encrypt($_POST["password"]);
-  $verif = $bdd->prepare("SELECT SQL_CALC_FOUND_ROWS `username` FROM `user` WHERE `username` = ?");
-  $verif->execute([$username]);
+		/* Recupere dans la base de donnee si le nom d'utilisateur est deja utilise ou non */
+		$password = encrypt($_POST["password"]);
+		$verif = $bdd->prepare("SELECT SQL_CALC_FOUND_ROWS `username` FROM `user` WHERE `username` = ?");
+		$verif->execute([$username]);
 
-  if($verif ->rowCount() == 0){
-    $ajout = $bdd->prepare("INSERT INTO `user` (`id`, `username`, `name`, `password`, `email`) VALUES (NULL ,?,?,?,?)");
-    $ajout->execute([$username ,$name, $password ,$email]);
-  }
+		if($verif ->rowCount() == 0){
+			$ajout = $bdd->prepare("INSERT INTO `user` (`id`, `username`, `name`, `password`, `email`) VALUES (NULL ,?,?,?,?)");
+			$ajout->execute([$username ,$name, $password ,$email]);
+		}
+	}
 }
 
 ?>
--- SignUp.php
+++ SignUp.php
@@ -18,8 +18,8 @@ include 'navbar.php';
 
       </div>
       <div class="row center">
-        <p>  Already an account ?
-          <a href="login.php"class="waves-effect waves-light btn">Log in !</a>
+        <p>  Déjà un compte ?
+          <a href="login.php"class="waves-effect waves-light btn">Connecte toi !</a>
         </p>
       </div>
 
@@ -29,79 +29,90 @@ include 'navbar.php';
 </div>
 
 
-
+	<!-- Partie à remplir pour l'inscription -->
     <div class="container">
       <div class = "customcont">
         <br><br>
-        <h5 class="header center teal-text text-lighten-2">Sign up</h5>
+        <h5 class="header center teal-text text-lighten-2">S'enregistrer</h5>
         <div class="row center">
           <form class="form-horizontal" method="post" action="Register.php">
 
             <div class="form-group">
-              <label for="name" class="cols-sm-2 control-label">Your Name</label>
+              <label for="name" class="cols-sm-2 control-label">Ton nom</label>
               <div class="cols-sm-10">
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                  <input type="text" class="form-control" name="name" id="name" required placeholder="Enter your Name"/>
+                  <input type="text" class="form-control" name="name" id="name" required placeholder="Entre ton nom"/>
                 </div>
               </div>
             </div>
 
             <div class="form-group">
-              <label for="email" class="cols-sm-2 control-label">Your Email</label>
+              <label for="email" class="cols-sm-2 control-label">Ton Email</label>
               <div class="cols-sm-10">
                 <div class="input-group">
                   <span id="checkemail" class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-                  <input type="email" class="form-control" name="email" id="email" required placeholder="Enter your Email"/>
+                  <input type="email" class="form-control" name="email" id="email" required placeholder="Entre ton email"/>
                 </div>
               </div>
             </div>
 
             <div class="form-group">
-              <label for="username" class="cols-sm-2 control-label">Username</label>
+              <label for="username" class="cols-sm-2 control-label">Pseudonyme</label>
               <div class="cols-sm-10">
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-                  <input type="text" class="form-control" name="username" id="username" required placeholder="Enter your Username"/>
+                  <input type="text" class="form-control" name="username" id="username" required placeholder="Entre ton pseudonyme"/>
                 </div>
               </div>
             </div>
 
             <div class="form-group">
-              <label for="password" class="cols-sm-2 control-label">Password</label>
+              <label for="password" class="cols-sm-2 control-label">Mot de passe</label>
               <div class="cols-sm-10">
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                  <input type="password" class="form-control" name="password" id="password" required placeholder="Enter your Password"/>
+                  <input type="password" class="form-control" name="password" id="password" required placeholder="Entre ton mot de passe"/>
                 </div>
               </div>
             </div>
 
             <div class="form-group">
-              <label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
+              <label for="confirm" class="cols-sm-2 control-label">Confirmation de mot de passe</label>
               <div class="cols-sm-10">
                 <div class="input-group">
                   <span id="confirmpassword" class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                  <input type="password" class="form-control" name="confirm" id="confirm" required placeholder="Confirm your Password"/>
+                  <input type="password" class="form-control" name="confirm" id="confirm" required placeholder="Confirme ton mot de passe"/>
                 </div>
               </div>
             </div>
 
+			
+			<div class="form-group">
+              <label for="confirm" class="cols-sm-2 control-label">Copier le captcha</label>
+              <br><br>
+			  <div class="cols-sm-10">
+					<img src="captcha.php" />
+                <div class="input-group">
+                  <span id="confirmpassword" class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                  <input type="text" class="form-control" name="captcha" id="captcha" required placeholder="Copier le captcha"/>
+                </div>
+              </div>
+            </div>
+			
             <br>
 
             <div class="form-group ">
               <span id="submit"></span>
               <input type="submit" class="btn btn-primary btn-lg btn-block login-button" value="Register"></input>
-            </div>
+            </div>			
           </form>
           <script src="js/register.js"></script>
-
-
-
         </div>
         <br><br>
       </div>
 </div>
+
 
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="js/materialize.js"></script>
