fab7ceffc779c7a0c44775477f80faeeff771a58
Myde <Mydexios.ms@gmail.com>
2018-06-25 13:23:45 +0200

    place holder

---
+++ confirm_poll.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+$_SESSION["current_page"] = "poll";
+include 'header.php';
+echo "<body>";
+
+
+include 'navbar.php';
+include 'db_connect.php';
+?>
+
+
+<div class="container">
+  <div class = "customcont">
+    <div class="row justify-content-center">
+      PlaceHolder
+    </div>
+  </div>
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+</body>
+</html>
