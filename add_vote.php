880098fe81119af886e1af5442364090be661ea4
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2018-06-27 22:30:36 +0200

    réparation bêtise ?

--- add_vote.php
+++ add_vote.php
@@ -4,7 +4,7 @@
   session_start();
   $mod = unserialize(base64_decode($_POST["modules"]));
 if(isset($_SESSION["username"])){
-echo "module# : type : Data <br>";
+//echo "module# : type : Data <br>";
 
 $idUser=null;
 $sql = 'SELECT id FROM user WHERE username=?';
@@ -17,10 +17,10 @@ while($row = $result->fetchColumn()) {
   foreach ($mod as $key => &$value) {
     $answer = $_POST["module".$key];
 
-      echo $key." : ".$value." : ";
+  //    echo $key." : ".$value." : ";
       switch($value){
         case "text":
-        echo $_POST["module".$key];
+    //    echo $_POST["module".$key];
 
         //insertion de la réponse ouverte dans la table
         $sql = "INSERT INTO answers (moduleId, data) VALUES (?,?)";
@@ -29,9 +29,9 @@ while($row = $result->fetchColumn()) {
         //récupération de l'id de la réponse ajoutée
         $sql = 'SELECT id FROM answers WHERE data=?';
         $result = $bdd->prepare($sql);
-        $result->execute([$_POST["module".$key]);
+        $result->execute([$_POST["module".$key]]);
         while($row = $result->fetchColumn()) {
-          echo $row;
+  //        echo $row;
           $textId=$row;
         }
         //on ajoute le vote en lui même
@@ -44,7 +44,7 @@ while($row = $result->fetchColumn()) {
         case "radio":
         $N = count($answer);
         for($i=0; $i < $N; $i++){
-          echo $answer[$i]." ";
+    //      echo $answer[$i]." ";
           //on insère le vote dans la table
           $sql = "INSERT INTO votes (userID, answerId) VALUES (?,?)";
           $result = $bdd->prepare($sql);
@@ -53,15 +53,38 @@ while($row = $result->fetchColumn()) {
         break;
         default :
         //TODO check timepicker
-        echo "default<br>";
+  //      echo "default<br>";
         break;
       }
-      echo "<br>";
+  //    echo "<br>";
   }
-  //on ajoute l'utilisateur dans la liste des votants 
+  //on ajoute l'utilisateur dans la liste des votants
   $sql = "INSERT INTO avote (userId, pollId) VALUES (?,?)";
   $result = $bdd->prepare($sql);
   $result->execute([$idUser,$_POST["pollid"]]);
+
+
+  //redirection
+  ?>
+  <div class="container">
+    <div class="customcont">
+      <h5 class="header center teal-text text-lighten-2">Information</h5>
+      <p>Votre vote a été ajouté avec succès. Vous allez bientôt être redirigé.</p>
+    </div>
+  </div>
+
+  <?php
+
+  $sql = " SELECT displayresult FROM polls WHERE id = ?";
+  $result = $bdd->prepare($sql);
+  $result->execute([$_POST["pollid"]]);
+  $row = $result->fetchColumn();
+  if($row){
+   header("refresh:5;url=results.php?id=".$_POST['pollid']);
+  }else{
+    header("refresh:5;url=index.php");
+  }
+
 
 }
 ?>
